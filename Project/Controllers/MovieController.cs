59cccc0c651cc248cc7dfc4a7eb8874a6284a967
Kvantym <144225547+Kvantym@users.noreply.github.com>
2025-06-19 19:08:21 +0300

    ребейз ревю гілки на initial commit

--- Project/Controllers/MovieController.cs
+++ Project/Controllers/MovieController.cs
@@ -104,13 +104,51 @@ namespace Project.Controllers
                 movie.ImageData = memoryStream.ToArray();
                 movie.ImageMimeType = Image.ContentType;
             }
-
             await _conectin.SaveChangesAsync();
 
             return RedirectToAction("MovieListPage", "Movie", new { message = "Фільм відредаговано" });
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> SortMovie(string filterField, string filterValue, string sortOrderYear, string sortOrderRating, int minYear, double minRating)
+        {
+            var movies = _conectin.movies.AsQueryable();
+
+            if(!string.IsNullOrEmpty(filterField)&& !string.IsNullOrEmpty(filterValue))
+            {
+                switch (filterField)
+                {
+                    case "Autor":
+                        movies = movies.Where(m => m.Autor.Contains(filterValue.ToLower()));
+                        break;
+                    case "Title":
+                        movies = movies.Where(m => m.Title.Contains(filterValue.ToLower()));
+                        break;
+                    case "Genre":
+                        movies = movies.Where(m => m.Genre.Contains(filterValue.ToLower()));
+                        break;
+                }
+            }
+
+            movies = movies.Where(m => m.Year >= minYear && m.Rating >= minRating);
+
+            if (sortOrderYear == "YearAsc") { movies = movies.OrderBy(m => m.Year); }
+            if (sortOrderYear== "YearDesc") { movies = movies.OrderByDescending(m => m.Year); }
+
+            if (sortOrderRating == "RatingAsc") { movies = movies.OrderBy(m => m.Rating); }
+            if (sortOrderRating == "RatingDesc") { movies = movies.OrderByDescending(m => m.Rating); }
+
+            ViewBag.filterField = filterField;
+            ViewBag.filterValue = filterValue;
+            ViewBag.sortOrderYear = sortOrderYear;
+            ViewBag.sortOrderRating = sortOrderRating;
+            ViewBag.minYear = minYear;
+            ViewBag.minRating = minRating;
+
+            return View("MovieListPage",movies.ToList());
 
         }
+
 
     }
 }
